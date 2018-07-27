b012153d9c836068d53975824153ccde8df7189b
bobi <boban0092@gmail.com>
2018-07-28 01:01:00 +0200

    Authentication system

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,7 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
+  config.action_mailer.default_url_options = { :host => "dev.yourhost.com" }
 
   # Show full error reports.
   config.consider_all_requests_local = true
